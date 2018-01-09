8ad7140eec41df40afd2ae2b437f526289b08cc3
Dev 5 <builov@inbox.ru>
2018-01-09 21:40:35 +0300

    add .txt

--- proj.php
+++ proj.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once 'vendor/autoload.php';
-//(()()()()))((((()()()))(()()()(((( )))))))
 //echo $argv[1];
 
+$str = file_get_contents($argv[1]);
+
 try {	
-	echo (MyLib::check_str($argv[1])) ? 'It`s ok' : 'String has error';	
+	echo (MyLib::check_str($str)) ? 'It`s ok' : 'String has error';	
 } catch (Exception $e) {
 	echo 'Error: ' . $e->getMessage();
 }
